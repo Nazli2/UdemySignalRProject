9d4451f09ee88c1938b2fdd23176a0c27347a929
Nazli2 <deniznazlisahin@gmail.com>
2025-09-16 23:52:43 +0300

    Apilerin Yazılması

---
+++ SignalRApi/Mapping/AboutMapping.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using SignalR.DtoLayer.AboutDto;
+
+namespace SignalRApi.Mapping;
+
+public class AboutMapping : Profile
+{
+    public AboutMapping()
+    {
+        CreateMap<AboutMapping, ResultAboutDto>().ReverseMap();
+        CreateMap<AboutMapping, GetAboutDto>().ReverseMap();
+        CreateMap<AboutMapping, CreateAboutDto>().ReverseMap();
+        CreateMap<AboutMapping, UpdateAboutDto>().ReverseMap();
+    }
+}
