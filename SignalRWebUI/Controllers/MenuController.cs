fa8ef4ed8853263e98d386c5847006a655b5c007
Nazli2 <deniznazlisahin@gmail.com>
2025-09-29 19:06:42 +0300

    Menü İşlemleri ve Ajax

---
+++ SignalRWebUI/Controllers/MenuController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.Dtos.CategoryDtos;
+using SignalRWebUI.Dtos.ProductDtos;
+
+namespace SignalRWebUI.Controllers;
+public class MenuController : Controller
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public MenuController(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    public async Task<IActionResult> Index()
+    { 
+        var client = _httpClientFactory.CreateClient();
+        var responseMessage = await client.GetAsync("https://localhost:7246/api/Product");
+        var responseMessage2 = await client.GetAsync("https://localhost:7246/api/Category");
+
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var jsonData2 = await responseMessage2.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultProductDto>>(jsonData);
+            var values2 = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData2);
+            ViewData["Category"] = values2.ToList();
+            return View(values);
+        }
+        return View();
+
+
+    }
+}
