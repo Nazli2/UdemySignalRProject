802e41c610468ee414a69b4c85468e9f7e4baaed
Nazli2 <deniznazlisahin@gmail.com>
2025-10-07 21:54:00 +0300

    Ders 107 - Sepet Ekranı Tasarımı

---
+++ SignalRWebUI/Dtos/BasketDtos/CreateBasketDto.cs
@@ -0,0 +1,10 @@
+﻿namespace SignalRWebUI.Dtos.BasketDtos;
+
+public class CreateBasketDto
+{
+    public decimal Price { get; set; }
+    public int Count { get; set; }
+    public decimal TotalPrice { get; set; }
+    public int ProductID { get; set; }
+    public int MenuTableID { get; set; }
+}
